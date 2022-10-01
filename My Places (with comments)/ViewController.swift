97dc291e8e4d43ed802d307ac800a4868563150c
Artyom Tyurmorezov <artyom9494@yandex.ru>
2022-10-01 10:11:45 +0300

    Initial Commit

---
+++ My Places (with comments)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My Places (with comments)
+//
+//  Created by Артём Тюрморезов on 01.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
