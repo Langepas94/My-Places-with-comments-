ad494c752f3bda2fee1f8101650ec8d082e5474d
Artyom Tyurmorezov <artyom9494@yandex.ru>
2022-10-06 22:02:52 +0300

    Add VC with map
    Also add 2 buttons - x and map

---
+++ My Places (with comments)/MapVCViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MapVCViewController.swift
+//  My Places (with comments)
+//
+//  Created by Артём Тюрморезов on 06.10.2022.
+//
+
+import UIKit
+
+class MapVCViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+    @IBAction func closeVC(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+
+
+}
